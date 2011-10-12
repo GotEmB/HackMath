4a211f222dfb40af2f3961a271b5ce1a88860194
Gautham Badhrinathan <b.gautham@gmail.com>
2011-10-12 23:25:41 +0400

    Initial Commit.

---
+++ iforSample/main.cpp
@@ -0,0 +1,35 @@
+//
+//  main.cpp
+//  iforSample
+//
+//  Created by Gautham Badhrinathan on 10/12/11.
+//  Copyright 2011 Code Kangaroos. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+void bigwork()
+{
+    int w, h, n;
+    cin >> w >> h >> n;
+    
+    //...
+}
+
+void dowork()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+        bigwork();
+}
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
